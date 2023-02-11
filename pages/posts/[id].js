2bcc3b0f9733b714630e5e5d07d2fc3ae0d251c0
Hasanowais3 <hasan.owais3@gmail.com>
2023-02-11 09:12:31 -0800

    complete again

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -4,6 +4,8 @@ import Head from 'next/head';
 import Date from '../../components/date';
 import utilStyles from '../../styles/utils.module.css';
 
+
+
 export async function getStaticProps({ params }) {
     const postData = await getPostData(params.id);
     return {
